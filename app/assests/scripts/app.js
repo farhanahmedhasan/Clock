fafa8d035142db2958032a1d83930d92f0a30687
Hasan <aaxhasandjmamunxyz2015@gmail.com>
2021-01-02 20:38:01 +0600

    Project Clock

---
+++ app/assests/scripts/app.js
@@ -0,0 +1,47 @@
+// setInterval(setClock, 1000)
+
+// const secondHand = document.querySelector('[data-second-hand]')
+// const minuteHand = document.querySelector('[data-minute-hand]')
+// const hourHand = document.querySelector('[data-hour-hand]')
+
+// function setClock() {
+//     const currentDate = new Date()
+//     const getSecond = currentDate.getSeconds() /60
+//     const getMinute = (getSecond + currentDate.getMinutes()) /60
+//     const getHour = (getMinute + currentDate.getHours()) /12
+
+//     setClockHandsInRotation(secondHand, getSecond)
+//     setClockHandsInRotation(minuteHand, getMinute)
+//     setClockHandsInRotation(hourHand,getHour)
+// }
+
+// function setClockHandsInRotation(element,rotation) {
+//     element.style.setProperty('--rotation', rotation * 360)
+// }
+// setClock()
+
+class Clock{
+    constructor() {
+        this.secondHand = document.querySelector('[data-second-hand]')
+        this.minuteHand = document.querySelector('[data-minute-hand]')
+        this.hourHand = document.querySelector('[data-hour-hand]')
+        setInterval(this.setClock(), 1000)
+        this.setClock()
+    }
+
+    setClock() {
+        const currentDate = new Date()
+        const getSecond = currentDate.getSeconds() /60
+        const getMinute = (getSecond + currentDate.getMinutes()) /60
+        const getHour = (getMinute + currentDate.getHours()) /12
+
+        setClockHandsInRotation(secondHand, getSecond)
+        setClockHandsInRotation(minuteHand, getMinute)
+        setClockHandsInRotation(hourHand,getHour)
+    }
+
+    setClockHandsInRotation(element, rotation) {
+        element.style.setProperty('--rotation', rotation * 360)
+    }
+}
+new Clock();
